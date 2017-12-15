59d45de7f57e10a76788ffc6cf467f196e2660aa
ChemiKyle <Kyle.2493@gmail.com>
2017-12-15 15:30:58 -0500

    SQL Sanitation

--- flask_search_kiosk.py
+++ flask_search_kiosk.py
@@ -1,21 +1,22 @@
 from flask import *
 import sqlite3
 import pandas as pd
-app = Flask(__name__)
 
+app = Flask(__name__)
 conn = sqlite3.connect('db/inv.db', check_same_thread = False)
 c = conn.cursor()
 
 
 def chem_search(phrase):
     results = []
-    for result in c.execute(str("SELECT Name,"
-                             "Quantity,"
-                             "Unit,"
-                             "Location,"
-                             "Sublocation"
-                             " FROM chem "
-              "WHERE Name LIKE '%" + phrase + "%'")):
+    cmd = ("SELECT Name, "
+    "Quantity, "
+    "Unit, "
+    "Location, "
+    "Sublocation "
+    "FROM chem "
+    "WHERE Name LIKE ?")
+    for result in c.execute(cmd, ['%' + phrase + '%']):
         results.append(result)
     return pd.DataFrame(results,
             columns=["Name", "Quantity", "Unit", "Location", "Shelf"])
@@ -30,8 +31,7 @@ def do_search():
     # TODO: Use radio buttons to select which table to search from
     # option = str(request.form['sub_db']) # Currently throws bad request error
     df = chem_search(phrase)
-    return render_template('view.html', tables = [df.to_html()],
-    titles = ['test'])
+    return render_template('view.html', tables = [df.to_html()])
 
 if __name__ == "__main__":
     app.run(debug=True)
