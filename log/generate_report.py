55233b1db9a60afa861d86b803de52bea28f7077
Kyle <kyle.2493@gmail.com>
2018-01-18 11:44:51 -0500

    Search term frequency plot operational

--- log/generate_report.py
+++ log/generate_report.py
@@ -3,6 +3,7 @@
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 from datetime import datetime as dt
 
@@ -17,7 +18,8 @@ def fetch_data(yr = '*', mnth = '*'):
 
 def hourly_x_daily(df):
     sns.jointplot("Day", "Hour", data = df, stat_func = None,
-            ylim = (24, 0), xlim = (1, 31))
+            ylim = (24, 0), xlim = (1, 31),
+            kind = "hex")
     plt.show()
 
 
@@ -33,18 +35,22 @@ def hourly(df):
 
 
 def top_terms(df):
-    search_terms = df["Phrase"].value_counts().head()
-    sns.barplot(search_terms)
+    srch = pd.DataFrame(df["Phrase"].value_counts().head(10))
+    srch.columns = ["Count"]
+    ax = sns.barplot(srch.index, srch.Count)
+    ax.set(xlabel = "Search Terms",
+            ylabel = "Times Searched")
+    ax.yaxis.set_major_locator(MaxNLocator(integer = True))
     plt.show()
 
 
 def main():
+    sns.set()
     d = dt.today()
     df = fetch_data(d.year, d.month)
-    hourly_x_weekday(df)
+    top_terms(df)
 
 
 if __name__ == "__main__":
-    sns.set()
     main()
 
