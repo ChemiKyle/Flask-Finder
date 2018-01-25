72551191ebaeac8823b4e9253421b7e53186c7cb
Kyle <kyle.2493@gmail.com>
2018-01-25 10:37:11 -0500

    Styling changes to plots, multiplotting

--- log/generate_report.py
+++ log/generate_report.py
@@ -20,35 +20,41 @@ def hourly_x_daily(df):
     sns.jointplot("Day", "Hour", data = df, stat_func = None,
             ylim = (24, 0), xlim = (1, 31),
             kind = "hex")
-    plt.show()
 
 
 def hourly_x_weekday(df):
-    sns.jointplot("Weekday", "Hour", data = df, stat_func = None,
-            ylim = (24, 0), xlim = (0, 6), color = "orange")
-    plt.show()
+    cmap = plt.cm.plasma
+    h_x_w = sns.jointplot("Weekday", "Hour", data = df, stat_func = None,
+            ylim = (24, 0), xlim = (0, 6), color = cmap(0.5), cmap = cmap,
+            kind = "hex",
+            marginal_kws = dict(bins = 7, rug = False),
+            joint_kws = dict(gridsize = 6))
+    plt.figure()
 
 
 def hourly(df):
     sns.distplot(df["Hour"]).set(xlim=(0,24))
-    plt.show()
 
 
 def top_terms(df):
     srch = pd.DataFrame(df["Phrase"].value_counts().head(10))
     srch.columns = ["Count"]
-    ax = sns.barplot(srch.index, srch.Count)
-    ax.set(xlabel = "Search Terms",
+    trms = sns.barplot(srch.index, srch.Count)
+    trms.set(xlabel = "Search Terms",
             ylabel = "Times Searched")
-    ax.yaxis.set_major_locator(MaxNLocator(integer = True))
-    plt.show()
+    trms.yaxis.set_major_locator(MaxNLocator(integer = True))
+    trms.set_xticklabels(trms.get_xticklabels(),
+            rotation = 30)
+    plt.figure()
 
 
 def main():
     sns.set()
     d = dt.today()
     df = fetch_data(d.year, d.month)
+    hourly_x_weekday(df)
     top_terms(df)
+    plt.show()
 
 
 if __name__ == "__main__":
