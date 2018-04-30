ebe56f2b9e1dca73362f4e654f011ccd96b5a174
Kyle <kyle.2493@gmail.com>
2018-04-30 14:31:30 -0400

    top_terms implemented but drawing blank

--- log/bokeh_report.py
+++ log/bokeh_report.py
@@ -1,34 +1,53 @@
 from generate_report import *
 from bokeh.io import output_file, show
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.transform import jitter
 
 df = unroll_dt(fetch_data())
-x = df.Weekday
-y = df.Hour + df.Minute / 60.
 
-# Causes weird offset
-DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+def hexbin_weekdays(df):
+    x = df.Weekday
+    y = df.Hour + df.Minute / 60.
 
-p = figure(title="Searches by Weekday and Time", match_aspect=True,
-           tools="wheel_zoom,reset", background_fill_color='#440154',
-           x_range=(0, 6),y_range=(24, 0))
-p.grid.visible = False
+    # Causes weird offset
+    DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
-r, bins = p.hexbin(x, y, size=0.5, hover_color="pink", hover_alpha=0.8)
+    p = figure(title="Searches by Weekday and Time", match_aspect=True,
+               tools="wheel_zoom,reset", background_fill_color='#440154',
+               x_range=(0, 6),y_range=(24, 0))
+    p.grid.visible = False
 
-#Jitter returns valueError
-p.circle(x=x, y=y,
-                color="white", size=2, alpha = 0.8)
+    r, bins = p.hexbin(x, y, size=0.5, hover_color="pink", hover_alpha=0.8)
 
-hover = HoverTool(tooltips=[("count", "@c")],
-                                  mode="mouse", point_policy="follow_mouse", renderers=[r])
+    #Jitter returns valueError
+    p.circle(x=x, y=y,
+             color="white", size=2, alpha = 0.8)
 
-p.add_tools(hover)
+    hover = HoverTool(tooltips=[("count", "@c")],
+                      mode="mouse", point_policy="follow_mouse", renderers=[r])
 
-# output_file("hexbin.html")
+    p.add_tools(hover)
 
-# p.y_range = Range1d(y.max(),y.min())
+    # output_file("hexbin.html")
 
-show(p)
+    # p.y_range = Range1d(y.max(),y.min())
+
+    show(p)
+
+def top_terms(df):
+    df = df[df['Phrase'] != ""]
+    srch = pd.DataFrame(df['Phrase'].value_counts().head(10))
+    srch.columns = ['Count']
+    srch['Phrase'] = srch.index
+    srch = ColumnDataSource(srch)
+    p = figure(x_range = (1,11), title="Most Searched Phrases")
+    p.vbar(source = srch, x = 'Phrase', top = 'Count',
+                    width = 0.9)
+#    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+    show(p)
+
+
+#hexbin_weekdays(df)
+top_terms(df)
